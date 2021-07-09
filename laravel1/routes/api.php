fb1e9d3f1f0f10d9c8c0bad4e58a8ea7282a2bfd
dali2000 <medalikebair@gmail.com>
2021-07-09 14:20:14 +0100

    adding Resful Api

--- laravel1/routes/api.php
+++ laravel1/routes/api.php
@@ -16,3 +16,17 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Get All employees
+Route::get('employees','EmployeeController@getEmployee');
+
+//get specefique employee
+Route::get('employee/{id}','EmployeeController@getEmployeeByid');
+
+//add employee
+Route::post('addEmployee','EmployeeController@addEmployee');
+//Update Employee
+Route::post('updateEmployee/{id}','EmployeeController@updateEmployee');
+//delete Employee
+Route::delete('deleteEmployee/{id}','EmployeeController@deleteEmployee');
