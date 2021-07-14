80f761cfee01623b68c8c629bd88e2378084faf3
dali2000 <medalikebair@gmail.com>
2021-07-14 12:09:29 +0100

    fix

--- Angular1/src/app/services/data.service.ts
+++ Angular1/src/app/services/data.service.ts
@@ -9,36 +9,36 @@ export class DataService {
   constructor(private httpClient:HttpClient) { }
 //emloyees
   getData(){
-    return this.httpClient.get('http://127.0.0.1:8000/api/employees');
+    return this.httpClient.get('http://127.0.0.1:8000/employees');
   }
   // submit(data){
   //   return this.httpClient.post('http://127.0.0.1:8000/api/addEmployee',data);
   // }
   deleteData(id){
-    return this.httpClient.delete('http://127.0.0.1:8000/api/deleteEmployee/'+id);
+    return this.httpClient.delete('http://127.0.0.1:8000/deleteEmployee/'+id);
   }
   getEmployeeById(id){
-    return this.httpClient.get('http://127.0.0.1:8000/api/employee/'+id);
+    return this.httpClient.get('http://127.0.0.1:8000/employee/'+id);
   }
   updateEmployee(id, data){
-    return this.httpClient.post('http://127.0.0.1:8000/api/updateEmployee/'+id,data);
+    return this.httpClient.post('http://127.0.0.1:8000/updateEmployee/'+id,data);
   }
 
 //user
 getDataUser(){
-  return this.httpClient.get('http://127.0.0.1:8000/api/users');
+  return this.httpClient.get('http://127.0.0.1:8000/users');
 }
 submit(data){
-  return this.httpClient.post('http://127.0.0.1:8000/api/addUser',data);
+  return this.httpClient.post('http://127.0.0.1:8000/addUser',data);
 }
 deleteUserData(id){
-  return this.httpClient.delete('http://127.0.0.1:8000/api/deleteUser/'+id);
+  return this.httpClient.delete('http://127.0.0.1:8000/deleteUser/'+id);
 }
 getUserById(id){
-  return this.httpClient.get('http://127.0.0.1:8000/api/user/'+id);
+  return this.httpClient.get('http://127.0.0.1:8000/user/'+id);
 }
 updateUser(id, data){
-  return this.httpClient.post('http://127.0.0.1:8000/api/updateUser/'+id,data);
+  return this.httpClient.post('http://127.0.0.1:8000/updateUser/'+id,data);
 }
 
 
